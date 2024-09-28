1bd19a4c3472c84f264acc7d409e7f5dcf1220ed
minhazapon <minhajulabedinapon@gmail.com>
2024-09-27 22:43:32 -0700

    added file

--- index.js
+++ index.js
@@ -4,8 +4,6 @@ require('dotenv').config()
 const app = express()
 const port = process.env.PORT || 5000
 
-//cardeal
-//cardeal1234
 
 console.log(process.env.DB_USERS)
 console.log(process.env.DB_PASS)
@@ -16,13 +14,13 @@ app.use(express.json());
 app.get('/', (req, res) => {
   res.send(' Car Deal Server ')
 })
-
+  
 
 ////////////////mongoDB///////////
+  
 
 
-
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USERS}:${process.env.DB_PASS}@cluster0.ruz4b.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -42,8 +40,96 @@ async function run() {
 
     //crud//
 
-     
+    const carCollection = client.db('carDB').collection('carData')
 
+    //create//
+      
+    app.post('/carData',  async(req, res) => {
+      
+         const carData = req.body 
+         console.log(carData) 
+         const result = await carCollection.insertOne(carData)
+         res.send(result)
+  
+    })
+
+    //create//
+
+
+    //read//
+
+    app.get('/carData',  async(req, res) => {
+      
+      const cursor = carCollection.find(); 
+      const result = await cursor.toArray()
+      res.send(result)
+
+    })
+
+    //read//
+
+
+    //delete//
+      
+    app.delete('/carData/:id',  async(req, res) => {
+      
+       const id = req.params.id 
+       const query = { _id: new ObjectId(id) }
+       const result = await carCollection.deleteOne(query)
+       res.send(result)
+
+    })
+    
+    //delete//
+
+
+    //update//
+      
+
+    app.get('/carData/:id',  async(req, res) => {
+      
+      const id = req.params.id 
+      const query = { _id: new ObjectId(id) }
+      const result = await carCollection.findOne(query) 
+      res.send(result)
+
+   })
+
+
+    app.put('/carData/:id',  async(req, res) => {
+      
+     
+          const id = req.params.id 
+          const UpUsers = req.body 
+          console.log(id, UpUsers)
+          const filter = { _id: new ObjectId(id) }
+          const option = {upsert: true}
+          const UpDateUsers = {
+            
+            $set: {
+
+              name: UpUsers.name ,
+              photourl: UpUsers.photourl,
+              brand: UpUsers.brand,
+              price: UpUsers.price,
+              category: UpUsers.category,
+              description: UpUsers.description,
+             
+
+
+            }
+
+          }
+
+          const result = await carCollection.updateOne(filter, option, UpDateUsers)
+          res.send(result)
+
+
+   })  
+
+ 
+
+    //update//
 
 
 
@@ -60,9 +146,9 @@ async function run() {
 run().catch(console.log);
 
 
+ 
 
-
-
+ 
 
 
 
